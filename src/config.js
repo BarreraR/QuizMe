45923d0b6ffebe815e641cf3abe15ba8bf937a11
Raul Barrera <rbarrera1992@gmail.com>
2021-02-02 20:04:55 -0600

    config update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  // API_ENDPOINT: '',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://capstone3-quizme.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'quizme-client-auth-token',
 }
